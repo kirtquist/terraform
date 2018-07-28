9b7af496c2946f51c4268e3365c43ce852d315e7
kirt <kirtnystrom@hotmail.com>
2018-07-28 12:03:55 -0700

    inital helloworld

---
+++ connections.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+#provider "google" {
+#    credentials = "${file("../google_account.json")}"
+#    project = "terraformlearning"
+#    region = "us-west1"
+#}
+
+#provider "asurerm" {
+#    subscription_id = "0"
+#    client_id = "1"
+#    client_secret = "2"
+#    tenant_id = "3"
+#}
